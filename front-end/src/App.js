646e517fd1695580781d0c5cabbd0d7da751759a
Emily Ryan <emilyryan@Emilys-MacBook-Pro.local>
2020-11-16 18:19:17 -0600

    signup updated

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,10 +1,12 @@
-import './App.css';
+import SignUp from "./components/signup";
+
 
 function App() {
   return (
-    <div className="App">
-       Hello
-    </div>
+   <SignUp/>
+
+
+
   );
 }
 
