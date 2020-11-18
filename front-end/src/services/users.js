c5542fd1c9ee83453c0594638ca4dc768c1e8976
April Darger <april.m.darger@gmail.com>
2020-11-17 19:57:15 -0700

    Mock up of a user profile page.

--- front-end/src/services/users.js
+++ front-end/src/services/users.js
@@ -1,2 +1,19 @@
 import axiosWithAuth from '../utils/axiosWithAuth'
 
+// April - Pass in id
+export const userById = id => {
+  return new Promise(resolve => {
+    axiosWithAuth()
+    .get(`/user/${id}`)
+    .then(res => {
+      console.log('userById res:',res);
+      
+      resolve(res)
+      
+    })
+    .catch(err => {
+      console.log('userById Error',err);
+      resolve()
+    })
+  })
+}
