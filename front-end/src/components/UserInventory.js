832c5ebf7949d7cc1fb4cf7f3e293522ff8c679a
Benaiah Varner <benaiahvarner@Benaiahs-MBP.home>
2020-11-18 17:22:12 -0500

    user

--- front-end/src/components/UserInventory.js
+++ front-end/src/components/UserInventory.js
@@ -1,17 +1,32 @@
-import React from 'react'
-import axios from 'axios';
+import React, {useState, useEffect} from 'react'
 import {axiosWithAuth} from '../utils/axiosWithAuth';
+import {useParams} from 'react-router-dom';
 
 const UserInventory = () => {
  
+const {id} = useParams()
+
+    useEffect(()=>{
         axiosWithAuth()
-        .get('https://african-marketplace-back-end.herokuapp.com/items')
+        .get('https://african-marketplace-back-end.herokuapp.com/users')
         .then((res) => {
             console.log('RES: ', res.data)
         })
         .catch((err) => {
             console.log('ERROR: ' ,err)
         })
+    },[])
+
+    // axiosWithAuth()
+    // .get(`https://african-marketplace-back-end.herokuapp.com/users`)
+    // .then((res)=>{
+    //     debugger
+    //     console.log('users item' ,res)
+
+    // .catch((err)=>{
+    //     debugger
+    //     console.log('Error' ,err)
+    // })
 
     return (
         <div>
